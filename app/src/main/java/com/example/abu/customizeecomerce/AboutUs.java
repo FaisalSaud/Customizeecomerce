6cfacf4c8869c5332bcd0ec57807bcf51f12b398
FaisalSaud <Fsa-1@hotmail.com>
2017-03-03 23:51:32 +0300

    Edit the size of text feilds.

--- app/src/main/java/com/example/abu/customizeecomerce/AboutUs.java
+++ app/src/main/java/com/example/abu/customizeecomerce/AboutUs.java
@@ -1,16 +1,17 @@
 package com.example.abu.customizeecomerce;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AboutUs extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
     }
 
-    //اذا شفتها قلي محمد خالد
-    //if you see it told me 
 }
