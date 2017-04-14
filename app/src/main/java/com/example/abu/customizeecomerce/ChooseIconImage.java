fbfaea4f768e11e1efd99be079b4caed2bc06032
MohammedK15 <m159.l159@gmail.com>
2017-04-14 12:54:57 +0300

    try to do things + fix some creat account + choose icon

--- app/src/main/java/com/example/abu/customizeecomerce/ChooseIconImage.java
+++ app/src/main/java/com/example/abu/customizeecomerce/ChooseIconImage.java
@@ -1,13 +1,157 @@
 package com.example.abu.customizeecomerce;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+import static android.R.attr.bitmap;
 
 public class ChooseIconImage extends AppCompatActivity {
+
+
+    ImageView icon1;
+    ImageView icon2;
+    ImageView icon3;
+    ImageView icon4;
+    ImageView icon5;
+    ImageView icon6;
+    ImageView icon7;
+    ImageView m;
+
+    Drawable icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_icon_image);
+
+        icon1 = (ImageView) findViewById(R.id.icon1);
+        icon2 = (ImageView) findViewById(R.id.icon2);
+        icon3 = (ImageView) findViewById(R.id.icon3);
+        icon4 = (ImageView) findViewById(R.id.icon4);
+        icon5 = (ImageView) findViewById(R.id.icon5);
+        icon6 = (ImageView) findViewById(R.id.icon6);
+        icon7 = (ImageView) findViewById(R.id.icon7);
+        m = (ImageView) findViewById(R.id.imageView);
+
+
+
+
+
+        icon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChooseIconImage.this,"111111111",Toast.LENGTH_LONG).show();
+                //icon1.buildDrawingCache();
+
+                icon1.setDrawingCacheEnabled(true);
+                Bitmap bit =icon1.getDrawingCache();
+
+                //icon1.setImageBitmap(bit);
+try {
+    Intent i = new Intent(ChooseIconImage.this,CreateAppStep1.class);
+    //Intent i = new Intent();
+
+    i.putExtra("icon", bitmap);
+    //startActivityForResult(i, 2);
+    setResult(Activity.RESULT_FIRST_USER, i);
+}catch(Exception e){
+    Toast.makeText(ChooseIconImage.this,e.getMessage(),Toast.LENGTH_LONG).show();
+}
+                finish();
+                //BitmapDrawable bd = (BitmapDrawable) icon1.;
+
+
+
+
+
+//                try {
+//                    icon = icon1.getDrawable();
+//
+//                    BitmapDrawable yourIcon = (BitmapDrawable) icon ;
+//
+//                    Toast.makeText(ChooseIconImage.this,"22222",Toast.LENGTH_LONG).show();
+//
+//
+//
+//                    Toast.makeText(ChooseIconImage.this,"333333333333",Toast.LENGTH_LONG).show();
+//                    Bitmap b = ((BitmapDrawable) icon).getBitmap();
+//
+//                    Toast.makeText(ChooseIconImage.this,"4",Toast.LENGTH_LONG).show();
+//                    i.putExtra("userIcon", b);
+//                    Toast.makeText(ChooseIconImage.this,"5",Toast.LENGTH_LONG).show();
+//                    setResult(2, i);
+//                    Toast.makeText(ChooseIconImage.this,"6",Toast.LENGTH_LONG).show();
+//                    finish();
+//                    Toast.makeText(ChooseIconImage.this,"7",Toast.LENGTH_LONG).show();
+//
+//                }catch (Exception e){
+//                    Toast.makeText(ChooseIconImage.this,e.getMessage(),Toast.LENGTH_LONG).show();
+//                }
+
+              /*
+                Bitmap b = ((BitmapDrawable) icon).getBitmap();
+                ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                b.compress(Bitmap.CompressFormat.PNG,100,bs);
+                i.putExtra("userIcon",bs.toByteArray());
+                finish();
+                */
+            }
+        });
+
+        icon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/abu/customizeecomerce/CreateAppStep1.java
+++ app/src/main/java/com/example/abu/customizeecomerce/CreateAppStep1.java
@@ -1,6 +1,7 @@
 package com.example.abu.customizeecomerce;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -8,7 +9,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +27,7 @@ public class CreateAppStep1 extends AppCompatActivity {
     EditText img;
     EditText storeName;
     String storeNameS;
+    Button chooseIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +35,24 @@ public class CreateAppStep1 extends AppCompatActivity {
         setContentView(R.layout.activity_create_app_step1);
 
         Button next = (Button) findViewById(R.id.nextButton);
-        Button chooseIcon = (Button) findViewById(R.id.chooseIconButton);
+        chooseIcon = (Button) findViewById(R.id.chooseIconButton);
         img = (EditText) findViewById(R.id.storeIcon);
         storeName = (EditText) findViewById(R.id.storeName);
         storeNameS = storeName.getText().toString();
 
         //img.setCompoundDrawables(null, null, getResources().getDrawable(R.drawable.icon),null);
 
-        chooseIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(CreateAppStep1.this, ChooseIconImage.class);
-                startActivity(i);
-            }
-        });
+//        chooseIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(CreateAppStep1.this, ChooseIconImage.class);
+//                startActivity(i);
+//            }
+//        });
 
         if (storeNameS.isEmpty()) {
 
-            Toast.makeText(CreateAppStep1.this, "The store name is empty", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(CreateAppStep1.this, "The store name is empty", Toast.LENGTH_SHORT).show();
         }else{
             try{
                 next.setOnClickListener(new View.OnClickListener() {
@@ -108,8 +112,9 @@ public class CreateAppStep1 extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         //Toast.makeText(this, "before switch", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(CreateAppStep1.this,"before switch",Toast.LENGTH_LONG).show();
         switch (requestCode){
+
             case SELECTED_PICTURE:
                 if (!(requestCode==RESULT_OK)){
 
@@ -144,11 +149,72 @@ public class CreateAppStep1 extends AppCompatActivity {
                 }
 //                Toast.makeText(this, "the if is false", Toast.LENGTH_LONG).show();
                 break;
+            case 2:
+                if (requestCode== Activity.RESULT_FIRST_USER){
+
+//                    try {
+//                        CharSequence userIcon3 = data.getCharSequenceExtra("userIcon");
+//
+//                        BitmapDrawable userIcon = (BitmapDrawable) BitmapDrawable.createFromPath(data.getStringExtra("userIcon"));
+//
+//                        Drawable userIcon1 = userIcon;
+//                        img.setBackground(userI8con1);
+//
+//                    }catch (Exception e){
+//                        Toast.makeText(CreateAppStep1.this, e.getMessage() + " Create app s1" , Toast.LENGTH_LONG).show();
+//                    }
+                    Toast.makeText(CreateAppStep1.this,"2",Toast.LENGTH_LONG).show();
+                    Intent i = new Intent();
+                    Bitmap bb = (Bitmap)  i.getParcelableExtra("icon");
+                    ImageView iv = (ImageView) findViewById(R.id.imageView) ;
+                    iv.setImageBitmap(bb);
+
+                    Drawable d =  iv.getDrawable();
+
+                    img.setBackground(d);
+
+//
+//                    Bitmap b = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("userIcon"),0,
+//                            getIntent().getByteArrayExtra("userIcon").length);
+//                    ImageView pr = new ImageView(this);
+//                    pr.setImageBitmap(b);
+//
+//                    Bitmap bitmap =(Bitmap) i.getParcelableExtra("icon");
+//                    //img.setImageBitmap(bitmap);
+//                    img.setBackground(bitmap);
+//
+//                    img.setBackground();
+
+
+                }
             default:
-//                Toast.makeText(this, "in default", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "in default", Toast.LENGTH_LONG).show();
                 break;
         }
     }
+
+    public void goToChooseIcon (View view){
+
+        try {
+            Intent i = new Intent(CreateAppStep1.this, ChooseIconImage.class);
+            startActivity(i);
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+//    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode==2){
+//            Parcelable[] userIcon2 = data.getParcelableArrayExtra("userIcon");
+//            CharSequence userIcon3 = data.getCharSequenceExtra("userIcon");
+//            Drawable userIcon = (Drawable) userIcon3;
+//            img.setBackground(userIcon);
+//
+//
+//        }
+//    }
 
 
 
