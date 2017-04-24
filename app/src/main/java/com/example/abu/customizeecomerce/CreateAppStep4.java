75e3a8e88437fa4143db87040c83dfa087ea3bcb
FaisalSaud <Fsa-1@hotmail.com>
2017-04-24 20:24:51 +0300

    -Active delaviry spinner

--- app/src/main/java/com/example/abu/customizeecomerce/CreateAppStep4.java
+++ app/src/main/java/com/example/abu/customizeecomerce/CreateAppStep4.java
@@ -1,5 +1,6 @@
 package com.example.abu.customizeecomerce;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,11 +19,13 @@ public class CreateAppStep4 extends AppCompatActivity {
     CheckBox Cash;
     CheckBox InStore;
     CheckBox byHand;
+    public static Activity step4;//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_app_step4);
+        step4=this;//test
         Button next = (Button) findViewById(R.id.NextStep4);
         PromoSlides = (CheckBox) findViewById(R.id.checkBox4);
         Mada = (CheckBox) findViewById(R.id.checkBox7);
@@ -35,18 +38,10 @@ public class CreateAppStep4 extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(Cash.isChecked() && Mada.isChecked()){
-                    UserMainPage.Payment=2;
-                }
-               else if(Cash.isChecked() && !Mada.isChecked()){
-                    UserMainPage.Payment=1;
-                }
-               else if(!Cash.isChecked() && Mada.isChecked()){
-                    UserMainPage.Payment=0;
-                }
                 Intent i = new Intent(CreateAppStep4.this, CreateAppStep5.class);
                 putOnMainPage();
                 startActivity(i);
+                if(UserMainPage.isCom)
                 finish();
 
             }
@@ -59,11 +54,32 @@ public class CreateAppStep4 extends AppCompatActivity {
         else
             UserMainPage.promoSlide.setVisibility(View.INVISIBLE);
 
-
         if(ShopingCart.isChecked())
             UserMainPage.ShoppingCart.setVisibility(View.VISIBLE);
         else
             UserMainPage.ShoppingCart.setVisibility(View.INVISIBLE);
+        if(Cash.isChecked() && Mada.isChecked()){
+            UserMainPage.Payment=2;
+        }
+        else if(Cash.isChecked() && !Mada.isChecked()){
+            UserMainPage.Payment=1;
+        }
+        else if(!Cash.isChecked() && Mada.isChecked()){
+            UserMainPage.Payment=0;
+        }
+        else
+            Toast.makeText(CreateAppStep4.this ,"Please Select a payment method" , Toast.LENGTH_SHORT).show();
+        if(InStore.isChecked() && byHand.isChecked()){
+            UserMainPage.Delivery=2;
+        }
+        else if(InStore.isChecked() && !byHand.isChecked()){
+            UserMainPage.Delivery=1;
+        }
+        else if(!InStore.isChecked() && byHand.isChecked()){
+            UserMainPage.Delivery=0;
+        }
+        else
+            Toast.makeText(CreateAppStep4.this ,"Please Select a delaviry method" , Toast.LENGTH_SHORT).show();
     }
 
     }
--- app/src/main/java/com/example/abu/customizeecomerce/PaymentAndDelaviry.java
+++ app/src/main/java/com/example/abu/customizeecomerce/PaymentAndDelaviry.java
@@ -8,33 +8,51 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class PaymentAndDelaviry extends AppCompatActivity {
-    public String[] arraySpinner;
+    public String[] ParraySpinner;
+    public String[] DarraySpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_and_delaviry);
         try{
             Spinner Payment =(Spinner) findViewById(R.id.Paymentspinner);
-            Spinner delivery =(Spinner) findViewById(R.id.Deliveryspinner);
+            Spinner Delivery =(Spinner) findViewById(R.id.Deliveryspinner);
 //          Payment.setPrompt("Select your payment methode needed");
 //          delivery.setPrompt("Select your payment methode needed");
-            this.arraySpinner = new String[3];
-            arraySpinner[0]="Please Select";
+            this.ParraySpinner = new String[3];
+            ParraySpinner[0]="Please Select";
           if (UserMainPage.Payment == 0) {
-              arraySpinner[1]="Mada";
-              arraySpinner[2]="";
+              ParraySpinner[1]="Mada";
+              ParraySpinner[2]="";
           }
           else if (UserMainPage.Payment == 1) {
-              arraySpinner[1]="Cash";
-              arraySpinner[2]="";
+              ParraySpinner[1]="Cash";
+              ParraySpinner[2]="";
           }
           else if (UserMainPage.Payment == 2) {
-              arraySpinner[1]="Mada";
-              arraySpinner[2]="Cash";
+              ParraySpinner[1]="Mada";
+              ParraySpinner[2]="Cash";
           }
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, arraySpinner);
-            Payment.setAdapter(adapter);
+            ArrayAdapter<String> Padapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, ParraySpinner);
+            Payment.setAdapter(Padapter);
+            this.DarraySpinner = new String[3];
+            ParraySpinner[0]="Please Select";
+            if (UserMainPage.Delivery == 0) {
+                DarraySpinner[1]="By hand";
+                DarraySpinner[2]="";
+            }
+            else if (UserMainPage.Delivery == 1) {
+                DarraySpinner[1]="In store";
+                DarraySpinner[2]="";
+            }
+            else if (UserMainPage.Delivery == 2) {
+                DarraySpinner[1]="By Hand";
+                DarraySpinner[2]="In Store";
+            }
+            ArrayAdapter<String> Dadapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, ParraySpinner);
+            Delivery.setAdapter(Dadapter);
         }
         catch(Exception e){
             Toast.makeText(PaymentAndDelaviry.this,e.getMessage(),Toast.LENGTH_LONG).show();
